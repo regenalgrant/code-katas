e156c031c921772e12db44ec55fded351af38589
regenalgrant <regenal@mac.com>
2016-12-11 16:29:49 -0800

    alltest

---
+++ test_parity_outlier.py
@@ -0,0 +1,8 @@
+"""Testing find_outlier.py from Codewars."""
+from find_outlier import find_outlier
+
+
+def test_find_outlier():
+    """Test the main function from find_outlier.py."""
+
+test.assert_equals(find_outlier([2,6,8,10,3]), 3)
