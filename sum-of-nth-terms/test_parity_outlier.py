d8b092b40dd649f27db44fa0e24970a6166a0bc1
Regenal Grant <regenalgrant@users.noreply.github.com>
2016-12-14 09:39:38 -0800

    Update test_parity_outlier.py

--- sum-of-nth-terms/test_parity_outlier.py
+++ sum-of-nth-terms/test_parity_outlier.py
@@ -1,8 +1,28 @@
-"""Testing find_outlier.py from Codewars."""
-from find_outlier import find_outlier
+"""Test the Sum_Series module."""
+from sum_series import series_sum
+
+# Basic tests
+assert series_sum(1) == "1.00"
+assert series_sum(2) == "1.25"
+assert series_sum(3) == "1.39"
+assert series_sum(4) == "1.49"
+assert series_sum(5) == "1.57"
+assert series_sum(6) == "1.63"
+assert series_sum(7) == "1.68"
+assert series_sum(8) == "1.73"
+assert series_sum(9) == "1.77"
+assert series_sum(15) == "1.94"
+assert series_sum(39) == "2.26"
+assert series_sum(58) == "2.40"
+assert series_sum(0) == "0.00"
 
 
-def test_find_outlier():
-    """Test the main function from find_outlier.py."""
-
-test.assert_equals(find_outlier([2,6,8,10,3]), 3)
+# Random tests
+def test_random():
+    
+    from random import randint
+    sol = lambda n: '0.00' if n == 0 else (lambda s: s[:-2] + "." + s[-2:])(str(int(round(sum([1.0 / (1 + i * 3) for i in range(n)]) * 100))))
+    for _ in range(40):
+        n = randint(0, 100)
+        print("Testing for " + str(n))
+        assert series_sum(n) == sol(n)
