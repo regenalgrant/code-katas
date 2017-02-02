23dd83b5d728eb228f9176a8c45ca8649e57c5fb
regenalgrant <regenal@mac.com>
2017-02-02 09:43:14 -0800

    commiting modified autocomplete

--- autocomplete.py
+++ autocomplete.py
@@ -2,14 +2,14 @@
 from trie import Trie
 
 
-class AutoComplete(object):
+class AutoComplete(object): # class
     """Python implementation of autocomplete class."""
-    def __init__(self, vocabulary, max_completions=5):
+    def __init__(self, vocabulary, max_completions=5): # initialise newly created object
         """Initialize the Autocomplete class."""
         self.vocabulary = Trie(iterable=vocabulary)
         self.max_completions = max_completions
 
-    def __call__(self, token):
+    def __call__(self, token): #method in the meta-class allows the class's instance to be called as a function
         """Takes user input and returns list of words, max five."""
         suggested_words = []
         if not isinstance(token, str):
@@ -17,11 +17,11 @@ class AutoComplete(object):
         start = self.vocabulary.root
         token = token
         completions = 0
-        while completions < self.max_completions:
+        while completions < self.max_completions: # loop
             while True:
                 if start.keys()[0] != '#':
                     start, token = get_word(start, token)
                 else:
                     suggested_words.append(token)
-                    break
+                    break #break loop
             return suggested_words
