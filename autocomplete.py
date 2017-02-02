0f3bce24b17207f065276eb4710c638dc16d9d42
regenalgrant <regenal@mac.com>
2017-02-02 09:41:20 -0800

    adding trie and the method of call

--- autocomplete.py
+++ autocomplete.py
@@ -2,9 +2,26 @@
 from trie import Trie
 
 
-class AutoCompleter(object):
+class AutoComplete(object):
     """Python implementation of autocomplete class."""
     def __init__(self, vocabulary, max_completions=5):
-        """Initialize the Autocompleter class."""
+        """Initialize the Autocomplete class."""
         self.vocabulary = Trie(iterable=vocabulary)
         self.max_completions = max_completions
+
+    def __call__(self, token):
+        """Takes user input and returns list of words, max five."""
+        suggested_words = []
+        if not isinstance(token, str):
+            return suggested_words
+        start = self.vocabulary.root
+        token = token
+        completions = 0
+        while completions < self.max_completions:
+            while True:
+                if start.keys()[0] != '#':
+                    start, token = get_word(start, token)
+                else:
+                    suggested_words.append(token)
+                    break
+            return suggested_words
--- test_autocomplete.py
+++ test_autocomplete.py
@@ -4,7 +4,7 @@ import autocomplete
 def test_get_word_token():
     """Tests that a word is returned as the token."""
     vocabulary = ['fix', 'fax', 'fit', 'fist', 'full', 'finch', 'final', 'finial']
-    start = autocomplete.AutoCompleter(vocabulary, max_completions=5)
+    start = autocomplete.AutoComplete(vocabulary, max_completions=5)
     start = start.vocabulary.root
     token = ''
     start, token = autocomplete.get_word(start, token)
@@ -13,7 +13,7 @@ def test_get_word_token():
 def test_get_word_start():
     """Tests that an ordered dictionary is returned as start."""
     vocabulary = ['fix', 'fax', 'fit', 'fist', 'full', 'finch', 'final', 'finial']
-    start = autocomplete.AutoCompleter(vocabulary, max_completions=5)
+    start = autocomplete.AutoComplete(vocabulary, max_completions=5)
     start = start.vocabulary.root
     token = ''
     start, token = autocomplete.get_word(start, token)
